4cbf18063f3911f68f866740f7500779dc9f4987
abhik137 <abhishek007prince@gmail.com>
2013-08-12 04:12:58 +0530

    minor edits & inclusions

---
+++ dsa/insert_sort.c
@@ -0,0 +1,23 @@
+/***** Incomplete *****/
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+	int a[5];
+	int i=0;
+	printf("Enter the array of 5 numbers to sort!\n");
+	while(i<5)
+	{
+		scanf("%d", &a[i]);
+		i++;
+	}
+/*
+	while(i<5)
+	{
+		printf("%d", a[i]);
+		i++;
+	}
+*/
+	return 0;
+}
