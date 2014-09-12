932de608d4369eacaaa3d9702b1353bc8323910e
Abhishek Kumar <abhishek.krpcs@gmail.com>
2014-09-12 19:16:38 +0530

    hackerrank maximizing-xor

--- hackerrank/algorithms/warmup/maximizing-xor/xor.py
+++ hackerrank/algorithms/warmup/maximizing-xor/xor.py
@@ -1,18 +1,20 @@
 #!/bin/python
-
 # Complete the function below.
 
-
-def  maxXor( l,  r):
-  pass
-
+def  maxXor(l, r):
+    maxv = 0
+    for i in xrange(l, r+1):
+        for j in xrange(l, r+1):
+            xor_val = i ^ j
+            if xor_val > maxv:
+                maxv = xor_val
+    return maxv
     
 
 _l = int(raw_input());
-
-
 _r = int(raw_input());
 
 res = maxXor(_l, _r);
 print(res)
 
+# https://www.hackerrank.com/challenges/maximizing-xor
