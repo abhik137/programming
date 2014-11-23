0942ee09a943428d3fa028a45aedccdb7373420e
Abhishek Kumar <abhishek.krpcs@gmail.com>
2014-11-23 14:58:50 +0530

    updated ptr_arr_basics.c, solved

--- C/pointers/ptr_arr_basics.c
+++ C/pointers/ptr_arr_basics.c
@@ -31,11 +31,17 @@ int main(void) {
 	printf("%s\n", str);
 	printf("%p\n", str);	// what is this doing? -> address of first char of str
 	printf("%p\n", str+1);	// address of second char of str
+	// printf("%s\n", str[1]);	// [segfault] warning: format ‘%s’ expects argument of type ‘char *’, but argument 2 has type ‘int’ [TODO]
+	printf("%c\n", str[1]); // ^coz '%s' expects 'char *' and we are giving a char to it, str[1] == *(str + 1)
 	printf("%p\n", &str);	// address to head of the string? NO, address of the pointer var str
-	printf("%p\n", &str+1);	// [TODO], difference of 8bytes b/w this and previous address value? [WHY]?
+	printf("%p\n", &str+1);	// [TODO], difference of 8bytes b/w this and previous address value? [WHY]? -> &str is the address of str, a pointer to char
+	printf("\nsizeof(char*) == %zd\n", sizeof(char*));	// coz , on my system, the size of a pointer to char is 8 bytes
 	printf("%d\n", *str);	// what integer is this? -> whatever can be made from 'N'=78
-	printf("%ld\n", sizeof(1));
-	// printf("%s\n", *str);	// [?]Wrong, [HOW][WHY], format ‘%s’ expects argument of type ‘char *’, but argument 2 has type ‘int’
+	printf("%c\n", *str);		// coz *str points to a character
+	printf("%d\n", *(str+1));	// 'i'=105, but why is it taking single byte ints [TODO](see next line link)
+	printf("%c\n", *(str+1));	// ^http://stackoverflow.com/questions/5045031/weird-result-when-use-d-specifier-to-print-a-unsigned-char-in-c
+	printf("%ld\n", sizeof(1));	// 4 bytes, as expected
+	// printf("%s\n", *str);	// [X]Wrong, [HOW][WHY][->line 40], format ‘%s’ expects argument of type ‘char *’, but argument 2 has type ‘int’[TODO]
 	
 	// int *f[5] = {213,214,215,216,217};	// [X]Wrong, this is array of pointers
 										// so the array should contain ptrs not values
@@ -72,9 +78,13 @@ void funcar(int ar[])	// passing an entire array, not memory efficient
  * http://www.thegeekstuff.com/2012/01/advanced-c-pointers/
  * http://www.cs.fsu.edu/~myers/c++/notes/pointers1.html
  * http://www.cs.fsu.edu/~myers/c++/notes/pointers2.html
+ * http://denniskubes.com/2012/08/16/the-5-minute-guide-to-c-pointers/
+ *
+ * problems solved: http://stackoverflow.com/questions/27086428/playing-with-pointers-dazed-and-confused-by-char-strings
  */
 
 /**
  * Notes:
  * -An array name is not a pointer. It's an identifier for a variable of type array, which has an implicit conversion to pointer of element type.
+ * '%s' expects 'const char*' argument
  */
