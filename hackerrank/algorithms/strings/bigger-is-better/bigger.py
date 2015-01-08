282747aeb4119644fbc19952b0e248bea9c013c8
Abhishek Kumar <abhishek.krpcs@gmail.com>
2015-01-09 00:17:19 +0530

    hackerrank bigger-is-better

---
+++ hackerrank/algorithms/strings/bigger-is-better/bigger.py
@@ -0,0 +1,24 @@
+#!/usr/bin/py
+# https://www.hackerrank.com/challenges/bigger-is-greater
+t = int(raw_input())
+for x in xrange(t):
+    s = list(raw_input())
+    orig_s = str(s)
+    target_i = None
+    target_j = None
+    for i in xrange(len(s)-1):
+        if s[i]<s[i+1]:
+            target_i = i
+    if target_i == None:
+        print "no answer"
+    else:    
+        for j in xrange(len(s)):
+            if s[target_i] < s[j]:
+                target_j = j
+        temp = s[target_i]
+        s[target_i] = s[target_j]
+        s[target_j] = temp
+        # s[(target_i+1): len(s)] = s[(target_i+1): len(s)].sort()
+        s[target_i+1: len(s)] = sorted(s[target_i+1: len(s)])
+        s = "".join(s)
+        print s
